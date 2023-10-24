900e5cb2df8401ea227b500246439c52962665d2
Ahmed Raza <ragix.aemi@gmail.com>
2023-10-24 11:58:47 +0500

    second commmit

---
+++ 02_class(string_fstring)/hello-world.py
@@ -0,0 +1,91 @@
+# Standard code (Professional)
+# name:str = "Ahmed Raza"
+# father_name:str = "Mehboob"
+
+
+# print("Hello")
+
+
+# print(name)
+
+
+#                                           Single Quotes
+# my_string: str = 'Hello, World!'
+# print(my_string)
+
+
+#                                            Double Quotes
+# my_string: str = "Hello, World!"
+# print(my_string)
+
+
+#                                            Triple Single Quotes
+# my_string: str = '''Hello,
+# World!'''
+# print(my_string)
+
+
+#                                              Triple Double Quotes
+# my_string: str = """Hello,
+# World!"""
+# print(my_string)
+
+
+
+#                                                String Formatting
+
+
+# Using %s and %d
+
+# name: str = "Alice"
+# age: int = 30
+# print("My name is %s and I am %d years old." % (name, age))
+
+
+# Using .format()
+
+# name: str = "Alice"
+# age: int = 30
+# print("My name is {} and I am {} years old.".format(name, age))
+
+
+# Using f-string (Python 3.6+)
+
+# name: str = "Alice"
+# age: int = 30
+# print(f"My name is {name} and I am {age} years old.")
+
+
+
+#                                           Advanced Techniques
+
+
+# f-string Expressions
+
+# x: int = 10
+# y: int = 20
+# print(f"The sum of {x} and {y} is {x+y}.")
+
+
+# f-string with Format Specification
+
+# pi: float = 3.14159
+# print(f"Value of pi to 2 decimal places: {pi:.2f}")
+
+
+
+#                                             Naming Conventions
+
+
+
+# Variables
+
+# my_variable: int = 10
+# print(my_variable)
+
+
+# Constants
+
+# PI: float = 3.14159
+# print(PI)
+
