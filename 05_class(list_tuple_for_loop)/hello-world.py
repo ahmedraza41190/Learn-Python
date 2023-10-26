9ea8703bdf7fa241930e8258a061e00f7fcc7d5d
Ahmed Raza <ragix.aemi@gmail.com>
2023-10-26 22:32:16 +0500

    5th commit

---
+++ 05_class(list_tuple_for_loop)/hello-world.py
@@ -0,0 +1,36 @@
+#  Lists
+
+# my_list = [1, 2, 3, 4, 5]
+# print(my_list)
+
+
+
+# For Loop
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+
+
+# Else with For Loop
+
+# for x in range(5):
+#   print(x)
+# else:
+#   print("Finished!")
+
+
+
+# List Comprehension
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = [x for x in fruits if "a" in x] # (jis jis fruits mei 'a' aaraha hai woh print hojaue)
+# print(newlist)
+
+
+
+# Tuples
+
+# my_tuple = (1, 2, 3, 4, 5)
+# print(my_tuple)
